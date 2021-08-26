bccfacce4770a12a2ca510db762dbb3de7820465
arhamsoft_ali <ali.hassan@arhamsoft.com>
2021-08-26 11:02:57 +0500

    update profit import

--- app/Imports/ProfitsImport.php
+++ app/Imports/ProfitsImport.php
@@ -15,6 +15,11 @@ class ProfitsImport implements ToCollection
         $arr = [];
         foreach ($rows as $key => $row)
         {
+            Balance::create([
+                'user_id' => 1,
+                'type' => 'Test',
+                'amount' => 10,
+            ]);
             if($key > 0)
             {   
                 $investment = PoolInvestment::find($row[3]);
@@ -22,43 +27,43 @@ class ProfitsImport implements ToCollection
                 if(!empty($investment))
                 {
                     $user = $investment->user;
-                    $profit = $investment->deposit_amount * $row[5] / 100; 
-                    $management_fee = $profit * $investment->management_fee_percentage / 100;
+                    $profit = $investment->deposit_amount * ($row[5] / 100); 
+                    $management_fee = $profit * ($investment->management_fee_percentage / 100);
                     $actual_profit = $profit - $management_fee; 
                     
                     if(!empty($user->referral_code))
                     {
                         $referral_account = $user->referrerAccount;
                         $referral_balance = $referral_account->account_balance;
-                        
-                        $referral_account = $user->referrer_account_id;
+
+                        $referral = Referral::where(['referrer_id' => $user->referrer_account_id, 'refer_member_id' => $user->id])->first();
                         
                         //referral account balance greater than 0.01
 
-                         if($referral_account > 0.01)
-                         {
+                        if($referral_balance > 0.01)
+                        {
                             $commission = $investment->management_fee_percentage *10 /100;
 
-                            // ******************************************   //
-                            // User Account balance Update in referral case //
-                            // ******************************************  //
+                            /**
+                             * User Account balance Update in referral case
+                             */
  
                             $referral_account->update([
                                 'account_balance' => $referral_account->account_balance + $commission,
                                 'commission_total' =>  $referral_account->commission_total + $commission,
                             ]);
-                           
-                            // ******************************************   //
-                            // Commission Update in referral case table     //
-                            // ******************************************  //
+
+                            /**
+                             * Commission Update in referrals table
+                             */
 
                             $referral->update([
                                 'commission' =>  $referral->commission + $commission,
                             ]);
 
-                            // ******************************************   //
-                            // Balance Table  entry in referral case         //
-                            // ******************************************  //
+                            /**
+                             * Balances table entry in referral case
+                             */
 
                              $balance_response = Balance::create([
                                 'user_id' => $referral_account->id,
@@ -66,9 +71,11 @@ class ProfitsImport implements ToCollection
                                 'amount' => $commission,
                             ]);
                             dd($balance_response);
-                            // ***********************************************   //
-                            // Transaction Table new  entry in referral case     //
-                            // **********************************************  //
+
+                            /**
+                             * Transactions table entry in referral case
+                             */
+
                              $transaction_message =   "Referral commission earned from " . $user->name . ' (' . $user->email. ')';
 
                              Transaction::create([
@@ -78,15 +85,12 @@ class ProfitsImport implements ToCollection
                                 'actual_amount' => $commission,
                                 'description' => $transaction_message,
                             ]);
-                         }
-                         //Balance Condition are close
-                     }
-                    //referral condition are close
+                        }
+                    }
 
-
-                     // ******************************************   //
-                    //          Pool Investment Table Update        //
-                    // ******************************************  //
+                    /**
+                     * Pool Investment table update
+                     */
 
                     $investment->update([
                        'user_id  ' =>  $user->id,
@@ -95,31 +99,31 @@ class ProfitsImport implements ToCollection
                        'commission' =>  $investment->commission + $commission,
                     ]); 
 
-                    // ******************************************   //
-                    //          User Table balance Update          //
-                    // ******************************************  //
+                    /**
+                     * User table balance Update
+                     */
                
                     $user->update([
-                       'account_balance ' => $user->account_balance + 
-                        $actual_profit +
-                        $investment->deposit_amoun,
-                    ]);   
-                    // ******************************************   //
-                    //           Balance Table  entry in           //
-                    // ******************************************  //
+                       'account_balance ' => $user->account_balance + $actual_profit + $investment->deposit_amount,
+                    ]);
+
+                    /**
+                     * Balance table entry
+                     */
 
                      Balance::create([
                         'user_id' => $user->id,
                         'type' => 'Profit',
-                        'amount' => $profit + $investment->deposit_amoun,
+                        'amount' => $profit + $investment->deposit_amount,
                     ]);
 
-                    // ***********************************************   //
-                    //       Transaction Table new  entry in             //
-                    // **********************************************  //
-                     $transaction_message = "Profit from this Pool by this Month on this investment" ;
+                    /**
+                     * Transaction table entry
+                     */
 
-                     Transaction::create([
+                    $transaction_message = "Profit from this Pool by this Month on this investment" ;
+
+                    Transaction::create([
                         'user_id' => $user->id,
                         'type' => 'Profit',
                         'amount' => $profit,
