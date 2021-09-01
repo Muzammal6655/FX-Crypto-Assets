4e2e2489228991f4eadebb7b82c1050180e61149
awaiskhalid1122 <awais.khalid@arhamsoft.com>
2021-09-01 18:06:46 +0500

    update dashboard and balances
    update dashboard and balances

--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -2,26 +2,6 @@
 @section('title', 'Dashboard')
 <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css">
 @section('content')
-<div class="main-wrapper">
-    <div class="container">
-        <div class="card-group">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
-                <div class="card-body dashboard-body">
-                    @if($user->photo_status == 0 && $user->passport_status == 0 )
-                        <p>Please upload your documents for account verification. 
-                        <a href="{{url('documents')}}" class="btn btn-primary" ><i class="fa fa-upload"></i> Upload Documents</a>
-                    @elseif(!CheckKYCStatus())
-                        <p>Your documents are under verification.Please wait for Admin approval.</p>
-                    @else
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Account Balance ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->account_balance,2)}}</strong></h5>
-                                    </div>
 <div class="container">
     <div class="card-group">
         <div class="card">
@@ -42,64 +22,42 @@
                                     <h5 class="card-title">Account Balance ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->account_balance,2)}}</strong></h5>
                                 </div>
                             </div>
-                            <div class="col-sm-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Total Commission ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->commission_total,2)}}</strong></h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Total Profits ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->profit_total,2)}}</strong></h5>
-                                    </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Commission ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->commission_total,2)}}</strong></h5>
                                 </div>
                             </div>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Total Deposits ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->deposit_total,2)}}</strong></h5>
-                                        <a href="{{ url('/deposits/create') }}" class="btn btn-success">Make New Deposit</a>
-                                        <a href="{{ url('/deposits') }}" class="btn btn-primary">Deposits History</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Total Withdrawals ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->withdraw_total,2)}}</strong></h5>
-                                        <a href="{{ url('/withdraws/create') }}" class="btn btn-success">Make New Withdraw</a>
-                                        <a href="{{ url('/withdraws') }}" class="btn btn-primary">Withdrawals History</a>
-                                    </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Profits ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->profit_total,2)}}</strong></h5>
                                 </div>
                             </div>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h3>Deposit History</h3>
-                                <canvas id="depositChart" style="width:100%;"></canvas>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Deposits ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->deposit_total,2)}}</strong></h5>
+                                    <a href="{{ url('/deposits/create') }}" class="btn btn-success">Make New Deposit</a>
+                                    <a href="{{ url('/deposits') }}" class="btn btn-primary">Deposits History</a>
+                                </div>
                             </div>
                         </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h3>Withdraw History</h3>
-                                <canvas id="withdrawChart" style="width:100%;"></canvas>
+                        <div class="col-sm-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Total Withdrawals ({{config('constants.currency')['symbol']}}): <strong>{{number_format($user->withdraw_total,2)}}</strong></h5>
+                                    <a href="{{ url('/withdraws/create') }}" class="btn btn-success">Make New Withdraw</a>
+                                    <a href="{{ url('/withdraws') }}" class="btn btn-primary">Withdrawals History</a>
+                                </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <h3>Investments History</h3>
-                                <canvas id="investmentsChart" style="width:100%;"></canvas>
-                            </div>
-                        </div>
-                    @endif
-                </div>
                     </div>
                 </div>
                 <br>
@@ -109,7 +67,7 @@
                             <form id="deposits-form" method="POST" action="{{url('/monthly-statements')}}" enctype="multipart/form-data">
                                 <div class="card-body">
                                 {{ csrf_field() }}
-                                    <input type="text" id='start_month'" name="start_month" value="" autocomplete="off">
+                                    <input type="text" id='start_month' name="start_month" value="" autocomplete="off">
                                     <input type="text" id='end_month' name="end_month" value=""  autocomplete="off">
                                     <button type="submit" class="btn btn-primary">Download</button>
 
--- resources/views/frontend/listing/balances.blade.php
+++ resources/views/frontend/listing/balances.blade.php
@@ -1,20 +1,6 @@
  @extends('frontend.layouts.app')
 @section('title', 'Balances')
 @section('content')
-<div class="main-wrapper">
-	<div class="container">
-		<div class="card">
-			<div class="card-header">
-				Balances History
-				<span class="pull-right">
-					<strong> Account Balance({{config('constants.currency')['symbol']}}):
-						{{number_format($user->account_balance,2)}}</strong>
-				</span>
-			</div>
-			<div class="card-body">
-				@include('frontend.messages')
-				<table class="table table-hover">
-					<thead>
 
 <div class="container">
 	<div class="card">
@@ -22,11 +8,8 @@
 			Balances History
 			<span class="pull-right">
 				 <strong> Account Balance({{config('constants.currency')['symbol']}}):
-				 	{{$user->account_balance}}</strong>
-					 <a href="{{url('/current-month-statement')}}" class="btn btn-success">Monthly Statement</a>
 				 	{{number_format($user->account_balance,2)}}</strong>
 			</span>
-			
 		</div>
 		<div class="card-body">
 			@include('frontend.messages')
@@ -43,37 +26,26 @@
 					@php $count = $balances->firstItem();  @endphp
 					@forelse($balances as $balance)
 						<tr>
-							<th scope="col">#</th>
-							<th scope="col">Type</th>
-							<th scope="col">Amount ({{config('constants.currency')['symbol']}})</th>
-							<th scope="col">Created At</th>
-						</tr>
-					</thead>
-					<tbody>
-						@php $count = $balances->firstItem();  @endphp
-						@forelse($balances as $balance)
-							<tr>
-								<th scope="row">{{ $count++ }}</th>
-								<td>{{ $balance->type }}</td>
-								@if( $balance->amount <= 0)
-									<td style="color:red;">{{ $balance->amount }}</td>
-								@else
-									<td style="color:green;">+{{ $balance->amount }}</td>
-								@endif
-								<td>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($balance->created_at), "UTC")->tz(auth()->user()->timezone)->format('d M, Y h:i:s A') }}</td>
-							</tr>
-						@empty
-							<tr>
-								<td>No records found!</td>
-							</tr>
-						@endforelse
-					</tbody>
-				</table>
-			</div>
-			<div class="card-footer text-center">
-				{{ $balances->links() }}
-			</div>
+							<th scope="row">{{ $count++ }}</th>
+							<td>{{ $balance->type }}</td>
+							@if( $balance->amount <= 0)
+								<td style="color:red;">{{ $balance->amount }}</td>
+							@else
+								<td style="color:green;">+{{ $balance->amount }}</td>
+							@endif
+							<td>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($balance->created_at), "UTC")->tz(auth()->user()->timezone)->format('d M, Y h:i:s A') }}</td>
+ 						</tr>
+					@empty
+    					<tr>
+    						<td>No records found!</td>
+    					</tr>
+					@endforelse
+				</tbody>
+			</table>
 		</div>
+		<div class="card-footer text-center">
+	    	{{ $balances->links() }}
+	  	</div>
 	</div>
 </div>
 @endsection
