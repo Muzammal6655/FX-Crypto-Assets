877b61a36b8648f40c389fd226e973d7d02efa88
anjumkhadija <76468864+anjumkhadija@users.noreply.github.com>
2021-09-01 17:09:25 +0500

    design changes

--- resources/views/frontend/pools/view.blade.php
+++ resources/views/frontend/pools/view.blade.php
@@ -3,36 +3,51 @@
 @section('content')
 <div class="main-wrapper">
 	<div class="container">
-		<div class="card-group">
+		<div class="card-group pool-view-des">
 			<div class="card">
 				<div class="card-body">
-					<h5 class="card-title text-center">{{$pool['name']}}</h5>
-					<p class="card-title">{{$pool['description']}}</p>
+					<h5 class="card-title card-top-title">{{$pool['name']}}</h5>
+					<p class="card-title text-center mb-4">{{$pool['description']}}</p>
 					<input type="hidden" class="form-control" name="pool_id" value="{{ $pool['id'] }}">
-					<span class="card-title">Days:<strong>{{$pool['days']}}</strong></span>
-					<br>
-					<span class="card-title">Wallet Address:<strong>{{$pool['wallet_address']}}
-					</strong></span>
-					<br>
-					<span class="card-title">Min Deposit:<strong>{{$pool['min_deposits']}}
-					</strong></span>
-					<br>
-					<span class="card-title">Max Deposit:<strong>{{$pool['max_deposits']}}
-					</strong></span>
-					<br>
-					<span class="card-title">Profit Percentage:<strong>{{$pool['profit_percentage']}}%
-					</strong></span>
-					<br>
-					<span class="card-title">Management Fee Percentage:<strong>{{$pool['management_fee_percentage']}}%
-					</strong></span>
-					<br>
-					<span class="card-title">Started Date:<strong>{{date('d M,Y', strtotime($pool['start_date']))}}
-					</strong></span>
+
+					<ul class="list-unstyled pool-body-info">
+						<li class="d-flex">
+							<span class="card-title">Days:</span>
+							<span class="card-detail">{{$pool['days']}}</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Wallet Address:</span>
+							<span class="card-detail">{{$pool['wallet_address']}}</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Min Deposit:</span>
+							<span class="card-detail">{{$pool['min_deposits']}}</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Max Deposit:</span>
+							<span class="card-detail">{{$pool['max_deposits']}}</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Profit Percentage:</span>
+							<span class="card-detail">{{$pool['profit_percentage']}}%</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Management Fee Percentage:</span>
+							<span class="card-detail">{{$pool['management_fee_percentage']}}%</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">Started Date:</span>
+							<span class="card-detail">{{date('d M,Y', strtotime($pool['start_date']))}}</span>
+						</li>
+						<li class="d-flex">
+							<span class="card-title">End Date:</span>
+							<span class="card-detail">{{date('d M,Y', strtotime($pool['end_date']))}}</span>
+						</li>
+					</ul>
 				</div>
 				<div class="card-footer">
-					<small class="text-muted">End Date: {{date('d M,Y', strtotime($pool['end_date']))}}</small>
 					<a href="{{ url('/deposits/create/?pool_id=' . Hashids::encode($pool->id)) }}" class="btn btn-xs btn-primary pull-right">Deposit</a> 
-					<a href="{{ url('/pools/' . Hashids::encode($pool->id)). '/invest' }}" class="btn btn-xs btn-primary pull-right">Invest</a>
+					<a href="{{ url('/pools/' . Hashids::encode($pool->id)). '/invest' }}" class="btn btn-xs btn-invest pull-right mr-2">Invest</a>
 				</div>
 			</div>
 		</div>
