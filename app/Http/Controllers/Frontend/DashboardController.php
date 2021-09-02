2dc0b2b404192f9c1742660c0f6167fa634c0cd9
raheelaShujaat <raheela.shujat@arhamsoft.com>
2021-09-02 11:58:02 +0500

    empty statment response

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -21,7 +21,7 @@ class DashboardController extends Controller
     public function index()
     {
         $data['user'] = auth()->user();
-        $data['total_investments'] = Transaction::where(['user_id' => auth()->user()->id,'type' =>'investment'])->sum('actual_amount');
+        $data['total_investments'] = Transaction::where(['user_id' => auth()->user()->id, 'type' => 'investment'])->sum('actual_amount');
 
         $withdraws = Withdraw::where(['user_id' => auth()->user()->id, 'status' => 1])->get();
         $deposits = Deposit::where(['user_id' => auth()->user()->id, 'status' => 1])->get();
@@ -51,7 +51,7 @@ class DashboardController extends Controller
             ->select(DB::raw('DATE_FORMAT(created_at,"%Y-%m") month'), DB::raw('sum(amount) as total_amount'))
             ->groupBy('month')->get();
 
-           
+
         $monthly_withdraws = Withdraw::where('user_id', auth()->user()->id)
             ->whereBetween('created_at', [$from, $to])
             ->select(DB::raw('DATE_FORMAT(created_at,"%Y-%m") month'), DB::raw('sum(amount) as total_amount'))
@@ -64,31 +64,32 @@ class DashboardController extends Controller
             ->groupBy('month')->orderby('approved_at')->get();
 
 
-       // dd($monthly_investments);
-     
+       
+        $monthly_statment=[];
+        if (!$monthly_deposits->isEmpty() || !$monthly_withdraws->isEmpty() || !$monthly_investments->isEmpty()) {
 
-        foreach($monthly_deposits as $deposits){
-            $monthly_statment[$deposits->month]['depositsmonth']= $deposits->month;
-        
-            $monthly_statment[$deposits->month]['total_deposits'] = $deposits->total_amount;
+            foreach ($monthly_deposits as $deposits) {
+                $monthly_statment[$deposits->month]['depositsmonth'] = $deposits->month;
+
+                $monthly_statment[$deposits->month]['total_deposits'] = $deposits->total_amount;
+            }
+
+            foreach ($monthly_withdraws as $withdraw) {
+                $monthly_statment[$withdraw->month]['total_withdraws'] = $withdraw->total_amount;
+            }
+            foreach ($monthly_investments as $investment) {
+                $monthly_statment[$investment->month]['total_monthly_investments'] = $investment->total_investment;
+                $monthly_statment[$investment->month]['total_monthly_investments_profit'] = $investment->total_profit;
+            }
         }
-      
-        foreach($monthly_withdraws as $withdraw){
-            $monthly_statment[$withdraw->month]['total_withdraws'] = $withdraw->total_amount;
+        if(count($monthly_statment)){
+        
+        $data['monthly_statment'] = $monthly_statment;
+         $pdf = LaravelPDF::loadView('pdfs.monthly_statement', $data);
+        return $pdf->download('monthly_statment ' . Carbon::now('UTC')->format('Y-m-d H.i.s') . '.pdf');
         }
-        foreach($monthly_investments as $investment){
-            $monthly_statment[$investment->month]['total_monthly_investments'] = $investment->total_investment;
-            $monthly_statment[$investment->month]['total_monthly_investments_profit'] = $investment->total_profit;
+        else{
+            return redirect()->back()->withErrors(['error' => "Sorry, you don't have any statement to download."]);
         }
-        
-       
-       
-        //dd($monthly_statment);
-        $data['monthly_statment']= $monthly_statment;
-        
-        
-        $pdf = LaravelPDF::loadView('pdfs.monthly_statement', $data);
-        return $pdf->download('invoice '.Carbon::now('UTC')->format('Y-m-d H.i.s').'.pdf');
-       
     }
 }
--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -10,6 +10,7 @@
                     Dashboard
                 </div>
                 <div class="card-body">
+                    @include('frontend.messages')
                     @if($user->photo_status == 0 && $user->passport_status == 0 )
                     <p>Please upload your documents for account verification.
                         <a href="{{url('documents')}}" class="btn btn-primary"><i class="fa fa-upload"></i> Upload Documents</a>
