493b59f89b2970b5ad8d138a16b8daaee780a86f
raheelaShujaat <raheela.shujat@arhamsoft.com>
2021-09-15 15:03:33 +0500

    muser login Middleware changes

--- app/Http/Middleware/UserCheckStatus.php
+++ app/Http/Middleware/UserCheckStatus.php
@@ -17,18 +17,25 @@ class UserCheckStatus
     public function handle($request, Closure $next)
     {
         $flag = false;
-        if(! $request->user()->status)
-        {
-            $message = 'Your account has been disabled. Please contact Admin in case of any concerns.';
+        //! $request->user()->status
+        //! $request->user()->is_approved
+        if ($request->user()->status != 1) {
+            if ($request->user()->status == 0)
+                $message = 'Your account has been disabled. Please contact Admin in case of any concerns.';
+            else if ($request->user()->status == 2)
+                $message = 'Your account is unverified. Please contact Admin in case of any concerns.';
+            else
+                $message = 'Your account has been deleted. Please contact Admin in case of any concerns.';
             $flag = true;
-        }
-        else if(! $request->user()->is_approved) {
-            $message = 'Your account is under review. Please contact Admin in case of any concerns.';
+        } else if ($request->user()->is_approved != 1) {
+            if ($request->user()->is_approved == 0)
+                $message = 'Your account is under review. Please contact Admin in case of any concerns.';
+            else
+                $message = 'Your account is rejected by admin. Please contact Admin in case of any concerns.';
             $flag = true;
         }
 
-        if($flag)
-        {
+        if ($flag) {
             Auth::guard()->logout();
             return redirect()->route('login')->withErrors(['error' => $message]);
         }
