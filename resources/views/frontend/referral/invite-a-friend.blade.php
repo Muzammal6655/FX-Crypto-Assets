71bc4904801e7959e4e03519230e0cbfc92300f3
raheelaShujaat <raheela.shujat@arhamsoft.com>
2021-09-16 15:55:55 +0500

    change referral code textfield to text area

--- resources/views/frontend/referral/invite-a-friend.blade.php
+++ resources/views/frontend/referral/invite-a-friend.blade.php
@@ -16,7 +16,14 @@
 
                     <!-- The text field -->
                     <div class="form-group">
-                        <input type="text" class="form-control" value="{{ url('/register?ref='.Hashids::encode($user->id)) }}" id="referral_code" readonly="">
+                        <!-- comment textfield due to referral code size issue in mobile view -->
+                        <!-- <input type="text" class="form-control" value="{{ url('/register?ref='.Hashids::encode($user->id)) }}" id="referral_code" readonly=""> -->
+                        <style>
+                            textarea {
+                                resize: none;
+                            }
+                        </style>
+                        <textarea rows="1" cols="20" class="form-control" id="referral_code" readonly="">{{ url('/register?ref='.Hashids::encode($user->id)) }}</textarea>
                     </div>
 
                     <!-- The button used to copy the text -->
@@ -27,42 +34,42 @@
                     </div>
                     <!-- ShareThis END -->
 
-                        <br>
-                        <h3>Referrals:</h3>
-                        <br>
-                        <div class="general-table-des">
-                            <div class="table-responsive">
-                                @include('frontend.messages')
-                                <table class="table table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col">Commission ({{config('constants.currency')['symbol']}})</th>
-                                            <th scope="col">Created At</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @php $count = $referrals->firstItem();  @endphp
-                                        @forelse($referrals as $referral)
-                                            <tr>
-                                                <th scope="row">{{ $count++ }}</th>
-                                                <td>{{ $referral->referMember->name }}</td>
-                                                <td>{{ $referral->referMember->email }}</td>
-                                                <td>{{number_format( $referral->commission,4)}}</td>
-                                                <td>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($referral->created_at), "UTC")->tz(auth()->user()->timezone)->format('d M, Y h:i:s A') }}</td>
-                                            </tr>
-                                        @empty
-                                            <tr>
-                                                <td>No records found!</td>
-                                            </tr>
-                                        @endforelse
-                                    </tbody>
-                                </table>
-                                {{ $referrals->links() }}
-                            </div>
+                    <br>
+                    <h3>Referrals:</h3>
+                    <br>
+                    <div class="general-table-des">
+                        <div class="table-responsive">
+                            @include('frontend.messages')
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Commission ({{config('constants.currency')['symbol']}})</th>
+                                        <th scope="col">Created At</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $count = $referrals->firstItem(); @endphp
+                                    @forelse($referrals as $referral)
+                                    <tr>
+                                        <th scope="row">{{ $count++ }}</th>
+                                        <td>{{ $referral->referMember->name }}</td>
+                                        <td>{{ $referral->referMember->email }}</td>
+                                        <td>{{number_format( $referral->commission,4)}}</td>
+                                        <td>{{ \Carbon\Carbon::createFromTimeStamp(strtotime($referral->created_at), "UTC")->tz(auth()->user()->timezone)->format('d M, Y h:i:s A') }}</td>
+                                    </tr>
+                                    @empty
+                                    <tr>
+                                        <td>No records found!</td>
+                                    </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                            {{ $referrals->links() }}
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -70,18 +77,18 @@
 </div>
 @endsection
 @section('js')
-    <script>
-        function copyToClipboard() {
-            /* Get the text field */
-            var copyText = document.getElementById("referral_code");
-            /* Select the text field */
-            copyText.select();
-            copyText.setSelectionRange(0, 99999); /* For mobile devices */
-            /* Copy the text inside the text field */
-            document.execCommand("copy");
-            /* Alert the copied text */
-            //alert("Copied the text: " + copyText.value);
-        }
-    </script>
-    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5d0b1d8072b51c001144b80b&product=inline-share-buttons' async='async'></script>
+<script>
+    function copyToClipboard() {
+        /* Get the text field */
+        var copyText = document.getElementById("referral_code");
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+        /* Alert the copied text */
+        //alert("Copied the text: " + copyText.value);
+    }
+</script>
+<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5d0b1d8072b51c001144b80b&product=inline-share-buttons' async='async'></script>
 @endsection
